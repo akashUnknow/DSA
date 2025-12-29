b6d3bb54a07e09038eff9d7ce40c49291f6d9168
akashUnknow <akash.unknow@gmail.com>
2025-12-29 20:10:34 +0530

    First Unique Character in a String

--- solutation/src/main/java/org/akash/solutation/Solutation.java
+++ solutation/src/main/java/org/akash/solutation/Solutation.java
@@ -1,19 +1,21 @@
-package org.akash.solutation;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+package main.java.org.akash.solutation;
+
+import java.util.HashMap;
 
 public class Solutation {
 
     public static void main(String[] args) {
 
-        List<Employee> employees = new ArrayList<>(List.of(
-                new Employee("Alice", 3, 70000, 30),
-                new Employee("Bob", 1, 50000, 25),
-                new Employee("Charlie", 2, 60000, 28)
-        ));
-
-        Collections.sort(employees, new NameAgeComparator()); 
-        System.out.println(employees);
+//        List<Employee> employees = new ArrayList<>(List.of(
+//                new Employee("Alice", 3, 70000, 30),
+//                new Employee("Bob", 1, 50000, 25),
+//                new Employee("Charlie", 2, 60000, 28)
+//        ));
+//
+//        Collections.sort(employees, new NameAgeComparator());
+//        System.out.println(employees);
+        Question question=new Question();
+        System.out.println(question.firstUniqChar("loveleetcode"));
     }
+
 }
